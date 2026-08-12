c12d879310cf111b7d6e86a9fcc53413718cd6ad
melike-bakirci <melike.bakirci@outlook.com>
2026-08-12 23:44:15 +0300

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/layout/Layout';
+import PageLoader from './components/ui/PageLoader';
 
-// Pages
-import Home from './pages/Home';
-import About from './pages/About';
-import KaroHali from './pages/KaroHali';
-import KaroHaliDetail from './pages/KaroHaliDetail';
-import CimHali from './pages/CimHali';
-import CimHaliDetail from './pages/CimHaliDetail';
-import Blog from './pages/Blog';
-import BlogDetail from './pages/BlogDetail';
-import Contact from './pages/Contact';
-import References from './pages/References';
-import Legal from './pages/Legal';
-import NotFound from './pages/NotFound';
+// Lazy Loaded Pages
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+const KaroHali = lazy(() => import('./pages/KaroHali'));
+const KaroHaliDetail = lazy(() => import('./pages/KaroHaliDetail'));
+const CimHali = lazy(() => import('./pages/CimHali'));
+const CimHaliDetail = lazy(() => import('./pages/CimHaliDetail'));
+const Blog = lazy(() => import('./pages/Blog'));
+const BlogDetail = lazy(() => import('./pages/BlogDetail'));
+const Contact = lazy(() => import('./pages/Contact'));
+const References = lazy(() => import('./pages/References'));
+const Legal = lazy(() => import('./pages/Legal'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 
 // Scroll to top on route change
 const ScrollToTop = () => {
@@ -30,27 +31,30 @@ const App: React.FC = () => {
     <BrowserRouter>
       <ScrollToTop />
       <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/hakkimizda" element={<About />} />
-          <Route path="/karo-hali" element={<KaroHali />} />
-          <Route path="/karo-hali/:slug" element={<KaroHaliDetail />} />
-          <Route path="/cim-hali" element={<CimHali />} />
-          <Route path="/cim-hali/:slug" element={<CimHaliDetail />} />
-          <Route path="/referanslar" element={<References />} />
-          <Route path="/referanslarimiz" element={<References />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:slug" element={<BlogDetail />} />
-          <Route path="/iletisim" element={<Contact />} />
-          <Route path="/gizlilik" element={<Legal defaultTab="privacy" />} />
-          <Route path="/kosullar" element={<Legal defaultTab="terms" />} />
-          <Route path="/kvkk" element={<Legal defaultTab="kvkk" />} />
-          <Route path="/cerez-politikasi" element={<Legal defaultTab="cookies" />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<PageLoader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/hakkimizda" element={<About />} />
+            <Route path="/karo-hali" element={<KaroHali />} />
+            <Route path="/karo-hali/:slug" element={<KaroHaliDetail />} />
+            <Route path="/cim-hali" element={<CimHali />} />
+            <Route path="/cim-hali/:slug" element={<CimHaliDetail />} />
+            <Route path="/referanslar" element={<References />} />
+            <Route path="/referanslarimiz" element={<References />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:slug" element={<BlogDetail />} />
+            <Route path="/iletisim" element={<Contact />} />
+            <Route path="/gizlilik" element={<Legal defaultTab="privacy" />} />
+            <Route path="/kosullar" element={<Legal defaultTab="terms" />} />
+            <Route path="/kvkk" element={<Legal defaultTab="kvkk" />} />
+            <Route path="/cerez-politikasi" element={<Legal defaultTab="cookies" />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </Layout>
     </BrowserRouter>
   );
 };
 
 export default App;
+
